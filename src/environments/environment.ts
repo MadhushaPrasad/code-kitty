23cdb71898de69180daa02ce73cd0ed16d5467f3
MadhushaPrasad <madushaprasad21@gmail.com>
2021-10-12 11:52:33 +0530

    chore(Env) : change envConfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyADRQa_QudNyokpOLAaf0-h82ChyQ_koM4',
     authDomain: 'code-kitty.firebaseapp.com',
